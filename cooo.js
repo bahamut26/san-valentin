854c0c2e8b0687803472f1442d689313a99e7b7a
bahamut26 <bahamut2026@gmail.com>
2025-02-09 21:30:09 -0400

    Update cooo.js

--- cooo.js
+++ cooo.js
@@ -1,49 +1,57 @@
-const urlSearchParams = new URLSearchParams(window.location.search);
-const messageCustom = urlSearchParams.get('message');
+const urlSearchParams = new URLSearchParams(window.location.search)
+
+const messageCustom = urlSearchParams.get('message')
 
 if (messageCustom) {
-  const mainMessageElement = document.querySelector('#mainMessage');
-  mainMessageElement.textContent = decodeURI(messageCustom);
+
+  const mainMessageElement = document.querySelector('#mainMessage')
+  mainMessageElement.textContent = decodeURI(messageCustom)
 }
 
-const btnOpenElement = document.querySelector('#open');
-const btnCloseElement = document.querySelector('#close');
 
-btnCloseElement.disabled = true;
+const btnOpenElement = document.querySelector('#open')
+const btnCloseElement = document.querySelector('#close')
 
-btnOpenElement.addEventListener('click', () => {
-  btnOpenElement.disabled = true;
-  btnCloseElement.disabled = false;
-  const coverElement = document.querySelector('.cover');
-  coverElement.classList.add('open-cover');
+btnCloseElement.disabled = true
 
-  setTimeout(() => {
-    coverElement.style.zIndex = -1;
-    const paperElement = document.querySelector('.paper');
-    paperElement.classList.remove('close-paper');
-    paperElement.classList.add('open-paper');
 
-    // Animación del corazón
-    const heartElement = document.querySelector('.heart');
-    heartElement.style.display = 'block';
-  }, 500);
-});
 
-btnCloseElement.addEventListener('click', () => {
-  btnOpenElement.disabled = false;
-  btnCloseElement.disabled = true;
+btnOpenElement.addEventListener('click', ()=> {
+  btnOpenElement.disabled = true
+  btnCloseElement.disabled = false
+  const coverElement = document.querySelector('.cover')
+  coverElement.classList.add('open-cover')
 
-  const coverElement = document.querySelector('.cover');
-  const paperElement = document.querySelector('.paper');
-  paperElement.classList.remove('open-paper');
-  paperElement.classList.add('close-paper');
+  setTimeout(()=>{
+    //
+    coverElement.style.zIndex = -1
+    
+    const paperElement = document.querySelector('.paper')
+    paperElement.classList.remove('close-paper')
+    paperElement.classList.add('open-paper')
 
-  setTimeout(() => {
-    coverElement.style.zIndex = 0;
-    coverElement.classList.remove('open-cover');
+    // animacion del corazón
+    const heartElement = document.querySelector('.heart')
+    heartElement.style.display = 'block'
+  
+  }, 500)
 
-    // Animación del corazón
-    const heartElement = document.querySelector('.heart');
-    heartElement.style.display = 'none';
-  }, 500);
-});
+})
+btnCloseElement.addEventListener('click', ()=> {
+  btnOpenElement.disabled = false
+  btnCloseElement.disabled = true
+
+  const coverElement = document.querySelector('.cover')
+  const paperElement = document.querySelector('.paper')
+  paperElement.classList.remove('open-paper')
+  paperElement.classList.add('close-paper')
+  
+  setTimeout(()=>{
+    coverElement.style.zIndex = 0
+    coverElement.classList.remove('open-cover')
+
+    // animacion del corazón
+    const heartElement = document.querySelector('.heart')
+    heartElement.style.display = 'none'
+  },500)
+})
